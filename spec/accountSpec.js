fc606b8d08f2207c932f77a1ed252479488c73dc
konradschlatte <konrad.schlatte@btinternet.com>
2017-03-06 14:05:46 +0000

    added deposit function as well as balance and transaction record

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,6 +1,22 @@
 describe('account', function(){
-    it('exists', function(){
-      account = new Account();
-      expect(account.balance).toBe(0);
-    });
+  account = new Account();
+  it('exists and has a balance', function(){
+      expect(account.balance).toBeDefined();
   });
+  it('has a record of deposits and withdrawals', function(){
+      expect(account.record).toEqual(jasmine.any(Array));
+  });
+});
+
+  describe('deposit money', function(){
+    it('has the possibility of adding money', function(){
+    expect(account.deposit).toBeDefined();
+  });
+    it('increases the balance when money is added', function(){
+    account.deposit(1000);
+    expect(account.balance).toBe(1000);
+  });
+    it('adds the deposit and the time in the record', function(){
+    expect(account.record[0].date).toEqual(jasmine.any(Date));
+    });
+});
---
+++ src/account.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Account = function(){
+  this.balance = 0;
+  this.record = [];
+};
+
+Account.prototype.deposit = function(money){
+  this.balance += money;
+  this.record.push({date : new Date(), value : money});
+};
