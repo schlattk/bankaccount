04b7f06a17fe212edb499929f3e8e5989da363ac
konrad <konrad.schlatte@gmail.com>
2019-01-28 13:07:48 +0000

    some more formatting

--- src/format.js
+++ src/format.js
@@ -3,8 +3,12 @@ class Format {
     let day = today.getDate().toString(),
     month = ( today.getMonth() + 1 ).toString(),
     year = today.getFullYear().toString();
-    if ( day.length === 1 ) { day = '0' + month };
-    if ( month.length === 1 ) { month = '0' + month };
+    if ( day.length === 1 ) {
+      day = '0' + month
+    };
+    if ( month.length === 1 ) {
+       month = '0' + month
+     };
     return day + '/' + month + '/' + year;
   };
   static statement( ledger ) {
@@ -13,10 +17,11 @@ class Format {
       let credit = '       ',
       debit = '       ',
       balance = item.balance.toFixed( 2 );
-      if ( item.amount >= 0 )
-      { credit = item.amount.toFixed( 2 ) }
-      else
-      { debit = Math.abs( item.amount ).toFixed( 2 ) };
+      if ( item.amount >= 0 ) {
+        credit = item.amount.toFixed( 2 )
+      } else {
+        debit = Math.abs( item.amount ).toFixed( 2 )
+      };
       console.log( item.date + '||  ' + credit + '  || ' + debit + '  || ' + balance );
       }
     );
